1f8b7c51bdd099ff018346b21e125eaecdb0ac42
zalasikR <jryanandrade33@gmail.com>
2025-09-02 10:42:06 -0300

    exercicios em andamento

---
+++ src/Segunda Lista de exercicios  em andamento/VerificaPalavra.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class VerificaPalavra {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Digite uma palavra: ");
+        String palavra = scanner.nextLine();
+
+
+        if (palavra.length() > 20) {
+            System.out.println("A palavra é muito grande.");
+        } else {
+            System.out.println("A palavra é pequena.");
+        }
+
+        scanner.close();
+    }
+}
---
+++ src/Segunda Lista de exercicios  em andamento/VerificarEmail.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class VerificarEmail {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um e-mail válido: ");
+        String email = scanner.nextLine();
+
+        if (email.contains("@") && email.endsWith(".com")) {
+            System.out.println("E-mail válido.");
+        } else {System.out.println("E-mail invalido, tente novamente: .");}
+        scanner.close();
+    }
+    }
